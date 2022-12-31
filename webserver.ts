970cd7a91c05b268a7cd1bd0578796464213b92b
girhty <z66t@yahoo.com>
2022-12-31 17:55:12 -0500

    thanks alot

--- webserver.ts
+++ webserver.ts
@@ -12,21 +12,29 @@ import { parseURL } from "https://deno.land/x/redis@v0.28.0/redis.ts";
     }
     return result.toString();
 }
-
+const regex = /([a-zA-Z,:,\/\/,0-9]*\.[a-zA-Z]*)/gm;
+const regex1= /[a-zA-Z0-9]/gm;
 const redis =  await connect(parseURL("redis://default:O9GvloCKjwfTR74NoCfM@containers-us-west-68.railway.app:8040"))
 const app = new Hono()
 app.get("/api", async (c) => {
   const val=makeid(5)
   const uri= c.req.queries("url")
-  const checker= await redis.exists(uri)
-  if (checker==1){
-    const qury=await redis.get(uri)
-    return c.redirect(qury, 301)
-  }
-  else{
-    await redis.setex(val,40,uri)
-    return c.json({url:`${"http://localhost:8000/api?url="+val}`});
-  }
+  const id=c.req.queries("id")
+  let m;
+const checker= await redis.exists(id)
+    if (checker==1){
+      const qury=await redis.get(id)
+      return c.redirect(`${qury || "https://"+qury}`, 301)
+    }
+    else{
+      while ((m = regex.exec(uri)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+      await redis.setex(val,20,m[0])
+      return c.json({url:`${"http://localhost:8000/api?id="+val}`});
+    }
+    }
 });
 
 serve(app.fetch)
