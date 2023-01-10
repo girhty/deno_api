2b39dbab87068ec1def3ba9de2035e01fbf8c02c
girhty <z66t@yahoo.com>
2023-01-10 16:19:44 -0500

    test

--- webserver.ts
+++ webserver.ts
@@ -27,7 +27,7 @@ const app = new Hono()
 app.use(
   '/api',
   cors({
-    origin: "https://smrf.netlify.app",
+    origin: "*",
     allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
     allowMethods: ['POST', 'GET', 'OPTIONS'],
     exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
