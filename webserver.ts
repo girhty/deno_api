487b552d98b7d4ad0ea9fe2791d7a5c6ed4f9692
girhty <z66t@yahoo.com>
2023-01-13 18:12:00 -0500

    wow

--- webserver.ts
+++ webserver.ts
@@ -67,7 +67,7 @@ app.all("/api", async (c) => {
     const val = {
       id: `${
         group[2]
-          ? btoa(group[2]).split("").reverse().join("").replace("=",'').substring(Math.floor(id.length/1.25),Math.floor(id.length/1.25)+6)
+          ? `${group[2].length>15 ? btoa(group[2]).split("").reverse().join("").replace("=",'').substring(Math.floor(length/1.25),Math.floor(length/1.25)+6): btoa(group[2]).split("").reverse().join("").replace("=",'').substring(0,6)}`
           : btoa(group[1]).substring(0, 6)
       }`,
       site: `${group[2] ? btoa(group[1] + group[2]) : btoa(group[1])}`,
